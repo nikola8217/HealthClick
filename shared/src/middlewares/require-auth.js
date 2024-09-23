4d21750759b43eb9cbfa2a9e73e69a19419e73f5
nikola8217 <gileer404@gmail.com>
2024-09-23 15:12:28 +0200

    Updates

---
+++ shared/src/middlewares/require-auth.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.requireAuth = void 0;
+const not_authorized_error_1 = require("../errors/not-authorized-error");
+const requireAuth = (req, res, next) => {
+    var _a;
+    const token = (_a = req.session) === null || _a === void 0 ? void 0 : _a.jwt;
+    if (!token) {
+        throw new not_authorized_error_1.NotAuthorizedError();
+    }
+    next();
+};
+exports.requireAuth = requireAuth;
