65c0f984473fc010e4c75242722a1689348cce2e
nikola8217 <gileer404@gmail.com>
2024-10-07 13:29:56 +0200

    Updates

--- shared/src/middlewares/require-auth.ts
+++ shared/src/middlewares/require-auth.ts
@@ -1,10 +1,9 @@
 import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
 import { NotAuthorizedError } from "../errors/not-authorized-error";
 
 export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
     const token = req.session?.jwt;
-    console.log('iz middlewara: ', token);
+    
     if (!token) {
         throw new NotAuthorizedError();
     }
