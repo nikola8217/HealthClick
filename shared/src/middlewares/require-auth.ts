7c720d71b026936a32c060d465fe6106ddc0c49c
nikola8217 <gileer404@gmail.com>
2025-01-16 21:58:44 +0100

    Updates

--- shared/src/middlewares/require-auth.ts
+++ shared/src/middlewares/require-auth.ts
@@ -2,9 +2,9 @@ import { Request, Response, NextFunction } from "express";
 import { NotAuthorizedError } from "../errors/not-authorized-error";
 
 export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
-    const token = req.session?.jwt;
+    const token = req.headers.authorization;
     
-    if (!token) {
+    if (!token || !token.startsWith("Bearer ")) {
         throw new NotAuthorizedError();
     }
 
