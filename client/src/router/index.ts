6509003043c21ad6f626d37f644425636000627d
nikola8217 <gileer404@gmail.com>
2025-01-18 19:05:29 +0100

    Front start

---
+++ client/src/router/index.ts
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import LoginView from '../views/LoginView.vue';
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'login',
+    component: LoginView
+  },
+  
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="authincation h-100" style="margin-top: 50px">
+        <div class="container h-100">
+            <div class="row justify-content-center h-100 align-items-center">
+                <div class="col-md-6">
+                    <div class="authincation-content">
+                        <div class="row no-gutters">
+                            <div class="col-xl-12">
+                                <div class="auth-form">
+                                    <h2 class="text-center mb-4 text-white">Login in to your account</h2>
+                                    <form action="index.html">
+                                        <div class="form-group">
+                                            <label class="mb-1 text-white"><strong>Email</strong></label>
+                                            <input type="email" class="form-control" style="color: black">
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="mb-1 text-white"><strong>Password</strong></label>
+                                            <input type="password" class="form-control" style="color: black">
+                                        </div>
+                                        <div class="text-center">
+                                            <button type="submit" class="btn bg-white text-primary btn-block">Login</button>
+                                        </div>
+                                    </form>
+                                    <div class="new-account mt-3">
+                                        <p class="text-white">Don't have an account? <a class="text-white" href="./page-register.html">Register here</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
