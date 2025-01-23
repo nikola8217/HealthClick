883b4b3ffa3013ca312caa4c50f7fa4a675850d2
nikola8217 <gileer404@gmail.com>
2025-01-23 23:10:56 +0100

    finishing

---
+++ client/src/conf.ts
@@ -0,0 +1,5 @@
+export const AUTH_ADDRESS = process.env.VUE_APP_AUTH_URL;
+export const SPECIALIZATIONS_ADDRESS = process.env.VUE_APP_SPECIALIZATIONS_URL;
+export const DOCTORS_ADDRESS = process.env.VUE_APP_DOCTORS_URL;
+export const PATIENTS_ADDRESS = process.env.VUE_APP_PATIENTS_URL;
+export const APPOINTMENTS_ADDRESS = process.env.VUE_APP_APPOINTMENTS_URL;
