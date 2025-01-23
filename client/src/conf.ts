903aca2b6022387bb2b32f97ff09cbad2b986d16
nikola8217 <gileer404@gmail.com>
2025-01-24 00:00:03 +0100

    fix

--- client/src/conf.ts
+++ client/src/conf.ts
@@ -1,5 +1,5 @@
-export const AUTH_ADDRESS = process.env.VUE_APP_AUTH_URL;
-export const SPECIALIZATIONS_ADDRESS = process.env.VUE_APP_SPECIALIZATIONS_URL;
-export const DOCTORS_ADDRESS = process.env.VUE_APP_DOCTORS_URL;
-export const PATIENTS_ADDRESS = process.env.VUE_APP_PATIENTS_URL;
-export const APPOINTMENTS_ADDRESS = process.env.VUE_APP_APPOINTMENTS_URL;
+export const AUTH_ADDRESS = 'http://afa444ea66ae1400bab66ad42b80b478-1097919717.eu-central-1.elb.amazonaws.com:5001';
+export const SPECIALIZATIONS_ADDRESS = 'http://a35086e262df448508ff3571c733205b-1712997127.eu-central-1.elb.amazonaws.com:5002';
+export const DOCTORS_ADDRESS = 'http://a8e4c9eb51c92480c9b105503d625ea8-801303990.eu-central-1.elb.amazonaws.com:5003';
+export const PATIENTS_ADDRESS = 'http://a5fd16e961f6b4d36a3789709adaa7ed-68616783.eu-central-1.elb.amazonaws.com:5004';
+export const APPOINTMENTS_ADDRESS = 'http://aab61b634634541639d622eb79892515-660187538.eu-central-1.elb.amazonaws.com:5005';
